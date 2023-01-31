d93eeec530845455447bddf3a83cf1544ce97d56
alex barlow <alex.barlow@rocketmail.com>
2023-01-31 14:02:56 -0500

    Added content to the landing page and styles

--- my-app/client/src/pages/LandingPage.js
+++ my-app/client/src/pages/LandingPage.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
+import 'materialize-css/dist/css/materialize.min.css';
+
+const styles = {
+    h1: {
+        color: '#FF7F50',
+        fontFamily: 'Hanalei Fill',
+        padding: '50px',
+        textAlign: 'left',
+
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tap-target');
+    var instances = M.TapTarget.init(elems, options);
+  });
 
 export default function LandingPage() {
-    return (
-        <div>
-
-        </div>
-    );
+  return (
+    <div>
+      <div className="container">
+        <h1 className='font-link' style={styles.h1}>Renting Made Easy</h1>
+        <p>
+          Login or Sign up to rent products or services near you! Want to start
+          making some extra dough! You too can add your items to the market
+          place.
+        </p>
+        <img src="Hotpot.png"></img>
+      </div>
+    </div>
+  );
 }
