52e9e3286cc2e604433c2e49e3b914c021849453
alex barlow <alex.barlow@rocketmail.com>
2023-01-31 13:28:24 -0500

    created header component and handled page change

--- my-app/client/src/components/Navbar.js
+++ my-app/client/src/components/Navbar.js
@@ -1,31 +1,43 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
 
 
 
 function Navbar({ currentPage, handlePageChange }) {
     return (
-       <ul className='nav nav-tabs'>
-        <li className='nav-items'>
-            <a href='#Home'
-            onClick={() => handlePageChange('Home')}
-            className={currentPage === 'Product' ? 'nav-link active'
-            : 'nav-link'}>Website Name</a>
+        <nav>
+        <div className='container'>
+            <ul>
+                <li className='nav-items'>
+                <a href='#Home'
+                onClick={() => handlePageChange('Home')}
+                className={currentPage === 'Product' ? 'nav-link active'
+                : 'nav-link'}>Website Name</a>
+                </li>
+            </ul>
+            <ul className='nav nav-tabs' id='nav-mobile' class='right hide-on-med-and-down'>
+                <li className='nav-items'>
+                <a href='#Products'
+                onClick={() => handlePageChange('Product')}
+                className={currentPage === 'Product' ? 'nav-link active'
+                : 'nav-link'}>Products</a>
+                </li>
+                <li className='nav-items'>
+                <a href='#Services'
+                onClick={() => handlePageChange('Services')}
+                className={currentPage === 'Services' ? 'nav-link active'
+                : 'nav-link'}>Services</a>
         </li>
-        <li className='nav-items'>
-            <a href='#Products'
-            onClick={() => handlePageChange('Product')}
-            className={currentPage === 'Product' ? 'nav-link active'
-            : 'nav-link'}>Products</a>
+        <li>
+            <button>
+                Log In Here!
+            </button>
         </li>
-        <li className='nav-items'>
-            <a href='#Services'
-            onClick={() => handlePageChange('Services')}
-            className={currentPage === 'Services' ? 'nav-link active'
-            : 'nav-link'}>Services</a>
-        </li>
-       </ul>
-       
-    );
+        </ul>
+    </div>
+    </nav>
+    )
 };
 
 export default Navbar;
+
