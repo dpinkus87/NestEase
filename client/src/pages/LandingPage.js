7654ff2f9947dd0f5afd52e98fb80a20e1ea7e11
alex barlow <alex.barlow@rocketmail.com>
2023-02-01 13:44:40 -0500

    fixed landing page not taking up whole page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -8,20 +8,23 @@ const styles = {
         padding: '50px',
         textAlign: 'center',
     },
+    height: {
+      height: '100vh',
+      justifyContent: 'center'
+    },
     container: {
         display: 'flex',
         flex: '50%',
         justifyContent: 'center',
         border: '5px solid rgba(0, 0, 0, 0.5)',
         boxShadow: '5px 5px 5px #00A6FB',
-        height: '100%'
-    }
+    },
 };
 
 export default function LandingPage() {
   return (
     <main>
-    <div className="flex-row justify-center">
+    <div className="flex-row justify-center" style={styles.height}>
       <div className="container" style={styles.container}>
         <div>
         <h1 className='font-link' style={styles.h1}>Renting Made Easy</h1>
