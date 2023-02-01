f340e1eb2a53e1901c738deb8ddf68e62aa8f93a
Jamie <asapharris88@gmail.com>
2023-02-01 13:27:59 -0500

    hooked up checkoutcard to checkout page for functionality

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import ProfilePage from '../components/Profile/Main'
 
 export default function ProfilePage() {
     return (
         <div>
-
+            <ProfilePage />
         </div>
     );
 }
