343272e3a64d7ff2156f4d022cc73fc903adf748
alex barlow <alex.barlow@rocketmail.com>
2023-02-01 15:29:39 -0500

    updated footer with working links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,41 +1,69 @@
 import React from "react";
-import { useLocation, useNavigate } from 'react-router-dom';
-import 'materialize-css/dist/css/materialize.min.css';
+import { useLocation, useNavigate } from "react-router-dom";
+import "materialize-css/dist/css/materialize.min.css";
 
 const styles = {
   color: {
-      background: '#051923'
-  }
-}
+    background: "#051923",
+   height: '100vh'
+  },
+  img: {
+    height: '35%',
+    width: '35%'
+  },
+};
 
 const Footer = () => {
   const navigate = useNavigate();
   const location = useLocation();
   return (
     <footer class="page-footer" style={styles.color}>
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Thank You For Choosing NestEase!</h5>
-                <p class="grey-text text-lighten-4">Rental Services are a quick and convienent way to make some extra money.</p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Developers</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/alexanderlbarlow/">Alexander Barlow</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Dylan Pinkus</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Jamie Harris</a></li>
-                </ul>
-              </div>
-            </div>
+      <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="white-text">Thank You For Choosing NestEase!</h5>
+            <p class="grey-text text-lighten-4">
+              Rental Services are a quick and convienent way to make some extra
+              money.
+            </p>
           </div>
-          <div class="footer-copyright">
-            <div class="container">
-            © 2023 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">ADD REPO LINK VIA FAVICON</a>
-            </div>
+          <div class="col l4 offset-l2 s12">
+            <h5 class="white-text">Developers</h5>
+            <ul>
+              <li>
+                <a
+                  class="grey-text text-lighten-3"
+                  href="https://www.linkedin.com/in/alexanderlbarlow/"
+                >
+                  Alexander Barlow
+                </a>
+              </li>
+              <li>
+                <a class="grey-text text-lighten-3" href="#!">
+                  Dylan Pinkus
+                </a>
+              </li>
+              <li>
+                <a class="grey-text text-lighten-3" href="#!">
+                  Jamie Harris
+                </a>
+              </li>
+            </ul>
           </div>
-        </footer>
+        </div>
+      </div>
+      <div class="footer-copyright" >
+        <div class="container">
+          Link to sites repo.
+          <a
+            class="grey-text text-lighten-4 right"
+            href="https://github.com/AlexanderBarlow/rental-site"
+          >
+            <img src="github-mark.png" alt="github logo" style={styles.img}></img>
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 };
 
