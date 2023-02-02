84da0311b9f8b3f8ac1177d3535f9bcd9b7859eb
alex barlow <alex.barlow@rocketmail.com>
2023-02-02 10:35:23 -0500

    added frontend mutations and queries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,28 @@
+import { gql } from '@apollo/client';
+
+export const ADD_PROFILE = gql`
+    mutation addProfile($name: String!, $password: String!, $city, String!){
+        addProfile(name: $name, password: $password, city, $city){
+            token
+            profile {
+                _id
+                name
+            }
+        }
+    }
+`;
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!){
+        login(email: $email, password: $password) {
+            token
+            profile {
+                _id
+                name
+            }
+        }
+    }
+`;
+
+export const ADD_PRODUCT = gql`
+    `
